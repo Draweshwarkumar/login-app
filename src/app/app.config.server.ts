197d03ba48e8e13b63788c4ca2fa3e93988e9b5c
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-10 19:39:45 +0530

    creating a login page

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import {MatButtonModule} from '@angular/material/button';
+
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    MatButtonModule
   ]
 };
 
