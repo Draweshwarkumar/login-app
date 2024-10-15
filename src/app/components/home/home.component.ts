6feee86420dbfd2eee0cb97c4cf66ed86d65edad
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-15 19:04:01 +0530

    creating home and login page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, RouterModule, MatIconModule,  MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, RouterModule, MatIconModule ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
