d89b0360c2690166a4c9168e86957f4eb2b3a416
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-16 19:45:22 +0530

    adding js

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule,MatInputModule,FormsModule],
+  imports: [MatFormFieldModule,MatInputModule,FormsModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  constructor() {}
 
+  ngOnInit(): void {
+      
+  }
 }
