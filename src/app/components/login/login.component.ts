692e359aa168aab32749df4ec78a2c6c581b4c2c
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-17 12:54:57 +0530

    generating token through frontend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { LoginService } from '../../service/login.service'; // Update the path if necessary
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule,MatInputModule,FormsModule,MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, CommonModule, HttpClientModule], // Add HttpClientModule here
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] // Fix styleUrl to styleUrls
 })
 export class LoginComponent implements OnInit {
   
-  constructor() {}
+  credentials = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {
-      
+    // Initialization logic here
+  }
+
+  onSubmit() {
+    if (this.credentials.username && this.credentials.password) {
+      console.log("We have to submit the form to server");
+      // Call the login method from the LoginService here
+    } else {
+      console.log("Fields are empty!!");
+    }
   }
 }
---
+++ src/app/service/login.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Import HttpClient
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  url = "http://localhost:8081";
+
+  constructor(private http: HttpClient) { } // Inject HttpClient here
+
+  generateToken(credentials: any) {
+    return this.http.post(`${this.url}/auth/login`, credentials); // Use HttpClient to make requests
+  }
+
+  loginUser(token: string) {
+    localStorage.setItem("token", token);
+    return true;
+  }
+
+  isLoggedIn() {
+    let token = localStorage.getItem("token");
+    return token !== undefined && token !== '' && token !== null; // Simplified
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    return true;
+  }
+
+  getToken() {
+    return localStorage.getItem("token");
+  }
+}
