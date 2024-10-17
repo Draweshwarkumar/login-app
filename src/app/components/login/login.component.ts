755a592ff944ed6c426bd67a33592d80d5c9f433
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-17 14:03:23 +0530

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,33 +4,45 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
-import { LoginService } from '../../service/login.service'; // Update the path if necessary
+import { LoginService } from '../../service/login.service';
+import { response } from 'express';
+import { error } from 'console';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, CommonModule, HttpClientModule], // Add HttpClientModule here
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'] // Fix styleUrl to styleUrls
+  styleUrls: ['./login.component.css'] // Use styleUrls, not styleUrl
 })
 export class LoginComponent implements OnInit {
   
   credentials = {
     username: '',
     password: ''
-  }
+  };
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService) {} // Inject the LoginService
 
   ngOnInit(): void {
-    // Initialization logic here
+     
   }
 
   onSubmit() {
-    if (this.credentials.username && this.credentials.password) {
+    if ((this.credentials.username !== '' && this.credentials.password !== '') && 
+        (this.credentials.username !== null && this.credentials.password !== null)) {
       console.log("We have to submit the form to server");
-      // Call the login method from the LoginService here
+      // You can call your login service here, e.g. this.loginService.doLogin(this.credentials);
+      this.loginService.generateToken(this.credentials).subscribe(
+        response=>{
+            console.log(response);
+        },
+        error=>{
+          console.log(error);
+
+        }
+      )
     } else {
       console.log("Fields are empty!!");
     }
